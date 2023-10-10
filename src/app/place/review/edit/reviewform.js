f16b3db4b1c9827845a71a494a3c87d596d13e3e
kiimnj <1najin@naver.com>
2023-10-10 17:53:07 +0900

    feat: review edit reviewform.js

---
+++ src/app/place/review/edit/reviewform.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <form>
+                <InputScore />
+                <InputReview />
+                <ReveiwButton />
+        </form>
+    </div>
+  )
+}
