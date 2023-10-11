3abbecc5836dc891622cbfde36b845c2c4538861
subin9804 <subin9804@naver.com>
2023-10-11 12:34:05 +0900

    fix: place page.js

--- src/app/place/layout.js
+++ src/app/place/layout.js
@@ -1,6 +1,8 @@
 import './place.css';
 
 export default function placeLayout({ children }) {
+
+  
     return (
       <div id="placeContainer">
         {children}
--- src/app/place/page.js
+++ src/app/place/page.js
@@ -5,51 +5,68 @@ import Link from "next/link";
 import Card from './card';
 import { useEffect, useState } from "react";
 
-let page = 1;
-const api = process.env.NEXT_PUBLIC_API_KEY
-const url = `https://api.visitjeju.net/vsjApi/contents/searchList?apiKey=${api}&locale=kr&page=${page}`
-const location = "home"
 
 export default function Index(props) {
     const [data, setData] = useState([]);
-    const [skip, setSkip] = useState(20);
-    let cnt = 20;
-    let limit = 20;
+    let [skip, setSkip] = useState(26);
+    let[isLoaded, setIsLoaded] = useState()
+    let limit = 26;
 
-
+    let addData = [];
+    
+    
+    /** 비짓제주 api 전체 페이지 가져오기 */
+    const api = process.env.NEXT_PUBLIC_API_KEY
+    const location = "home"
+    
     useEffect(() => {
-        getData(url)
-        .then(data => {
-            let newData = data.items;
-            setData(newData)
-        })
-        .catch(error => console.log(error))
-    }, [skip])
-    let items = data.slice((skip === 20 ? 0 : skip - 20), skip);
 
-    const pageRight = () => {
-        if(skip === 100) {
-            setSkip(20)
-            cnt = 20;
-            page++;
-        } else {
-            cnt += limit
-            setSkip(cnt);
+        for (let i = 1; i < 53; i++) {
+            // let i = 1
+            const url = `https://api.visitjeju.net/vsjApi/contents/searchList?apiKey=${api}&locale=kr&page=${i}`
+            getData(url)
+            .then((resp) => {
+                let items = resp.items
+                let newData = items.filter(item => item.alltag != null); // data에서 유효한 데이터만 필터링
+                return newData;
+            })
+            .then(newData => {
+                addData.push(...newData)
+                
+            })            
+            .finally(() => setData(addData))   
         }
-        
-        console.log(skip)
+    }, [])
+    
+
+    console.log("data:", data.length)
+    console.log(skip)
+    // console.log(skip)
+
+    // 페이지 분할
+    let list;
+    if(skip === limit) {                // 첫페이지일 경우
+        list = data.slice(0, skip);
+    } else if(skip > data.length) {     // 마지막 페이지일 경우
+        list = data.slice(skip - limit)
+    } else {                            // 첫페이지도, 마지막 페이지도아닐 경우
+        list = data.slice(skip - limit, skip);
+    }
+
+    let pageRight = () => {
+        if(skip< data.length) {
+            setSkip(skip += limit)
+        }
     }
 
     const pageLeft = () => {
-        if(skip === 0 && page != 1) {
-            setSkip(100);
-            cnt = 100;
-            page--;
+        if(skip != limit) {
+            setSkip(skip -= limit)
         } else {
-            cnt -= limit;
-            setSkip(cnt);
+            setSkip(limit)
         }
     }
+
 
     return(
         <>
@@ -59,43 +76,42 @@ export default function Index(props) {
             </div>
 
             <ul id="quick" className="nav">
-                <li>
-                    <div className="qmenu"></div> <p>공지사항</p>
-                </li>
-                <li>
-                    <div className="qmenu"></div> <p>커뮤니티</p>
-                </li>
-                <li>
-                    <div class="qmenu"></div> <p>고객센터</p>
-                </li>
-                <li>
-                    <div class="qmenu"></div> <p>나의여행</p>
-                </li>
-                <li>
-                    <div class="qmenu"></div> <p>공지사항</p>
-                </li>
+                <li><div className="qmenu"></div> <p>공지사항</p></li>
+                <li><div className="qmenu"></div> <p>커뮤니티</p></li>
+                <li><div className="qmenu"></div> <p>나의지도</p></li>
+                <li><div className="qmenu"></div> <p>고객센터</p></li>
+                <li><div className="qmenu"></div> <p>공지사항</p></li>
             </ul>
 
             <hr/>
 
             
             <div id="cardContainer">
-                {items.map(item => (
+                {list.map(item => (
                     <div key={item.contentsid}>
                         <Link href={`/place/${item.contentsid}`}>
                         <Card 
                             id={item.contentsid}
                             title={item.title}
-                            photo={item.repPhoto.photoid.imgpath}
-                            thumb={item.repPhoto.photoid.thumbnailpath}
+                            photo={item.repPhoto === null ? null : item.repPhoto.photoid.imgpath}
+                            thumb={item.repPhoto === null ? null : item.repPhoto.photoid.thumbnailpath}
                             tag={item.tag}
                             script={item.introduction}
                         />
                         </Link>
                     </div>
                 ))}
-                <button onClick={pageLeft}>left</button>
-                <button onClick={pageRight}>right</button>
+                {(list.length % 2 == 1 && skip >= data.length) &&
+                    <div className="emptyCard"></div>
+                }
+                <div>
+                    <button onClick={pageLeft} className="btn btn-secondary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-left-fill" viewBox="0 0 16 16"><path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/></svg>    
+                    </button>
+                    <button onClick={pageRight} className="btn btn-secondary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-right-fill" viewBox="0 0 16 16"><path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/></svg>
+                    </button>
+                </div>
             </div>
         </>
     )
