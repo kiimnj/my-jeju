6cc3a8011f7633654c47626ce98125ce70b3302f
kiimnj <1najin@naver.com>
2023-10-12 09:59:43 +0900

    fit: review/edit/reviewform.js 컴포넌트 구성으로 변경

--- src/app/place/review/edit/reviewform.js
+++ src/app/place/review/edit/reviewform.js
@@ -1,10 +1,16 @@
+import ReveiwButton from './reveiwbutton'
+import ReviewText from './reviewtext'
+import AddImg from './addimg'
+
 export default function ReviewForm() {
   return (
     <div id="reviewForm">
-        <form>
-            <input type="text" placeholder="리뷰를 작성하세요."></input>
-            <br />
-            <button className="reviewButton">리뷰 등록하기</button>
+        <form >
+          <div className="wrap">
+            <ReviewText />
+            <AddImg />
+            <ReveiwButton />
+          </div>
         </form>
     </div>
   )
