b47cae28dcee0da11a8eda58d6a6a4f0f5ede12f
kiimnj <1najin@naver.com>
2023-10-12 13:43:18 +0900

    feat: review/reviewfooter.js

---
+++ src/app/place/review/reviewfooter.js
@@ -0,0 +1,13 @@
+
+export default function ReviewFooter() {
+    return (
+            <div class="reveiwFooter">
+                <span class="commentIcon"><i class="fa-regular fa-comment"></i></span>
+                <span class="commentCnts">2</span>
+                <p class=''></p>
+
+                <span class="recommendIcon"><i class="fa-regular fa-thumbs-up"></i></span>
+            <span class="recommendCnts">2</span>
+        </div>
+    )
+}
