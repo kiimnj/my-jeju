4a145c3075755dcb623ab654c01293771eb22a45
kiimnj <1najin@naver.com>
2023-10-12 09:52:00 +0900

    fix: review/placetab.js class,id,icon추가

--- src/app/place/review/placetab.js
+++ src/app/place/review/placetab.js
@@ -1,15 +1,13 @@
 export default function PlaceTab ({id, title, photo, thumb, tag, script}) {
     return (
-        <div>
-            <div style={{"width": "60px", "height": "60px", "flexShrink":"0", "border":"1px solid black"}}>
-                <img src={photo} alt="..." style={{"width": "60px", "height": "60px"}} />
-            </div>            
-                <div>
-                    <div class="title">title</div>
-                    <div class="score">★★★★★ 5.0</div>
+        <div id="wrapper">
+                <img className="items" id="thumb" src={photo} alt="..." style={{"width": "60px", "height": "60px", "border":"1px solid black"}} />
+                <div className="items">
+                    <div id="title">title</div>
+                    <div id="score">★★★★★ 5.0</div>
                 </div>
-                <span class="visited"><img src="" alt="icon" /></span>
-                <span class="liked"><img src="" alt="icon" /></span>
+                <div className="items" id="visited"><i class="icon fa-solid fa-circle-check"></i></div>
+                <div className="items" id="liked"><i class="icon fa-solid fa-heart"></i></div>
         </div>
     )
 }
