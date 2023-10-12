b9efd70dd7ad4bc8337bf226971ae871e404f921
kiimnj <1najin@naver.com>
2023-10-12 13:46:24 +0900

    fix: review/placetab.js >모양 아이콘 추가

--- src/app/place/review/placetab.js
+++ src/app/place/review/placetab.js
@@ -1,13 +1,14 @@
 export default function PlaceTab ({id, title, photo, thumb, tag, script}) {
     return (
         <div id="wrapper">
-                <img className="items" id="thumb" src={photo} alt="..." style={{"width": "60px", "height": "60px", "border":"1px solid black"}} />
-                <div className="items">
-                    <div id="title">title</div>
-                    <div id="score">★★★★★ 5.0</div>
-                </div>
-                <div className="items" id="visited"><i class="icon fa-solid fa-circle-check"></i></div>
-                <div className="items" id="liked"><i class="icon fa-solid fa-heart"></i></div>
+            <img className="items" id="thumb" src={photo} alt="..." style={{"width": "60px", "height": "60px", "border":"1px solid black"}} />
+            <div className="items">
+                <div id="title">title</div>
+                <div id="score">★★★★★ 5.0</div>
+            </div>
+            {/* <div className="items" id="visited"><i class="icon fa-solid fa-circle-check"></i></div>
+            <div className="items" id="liked"><i class="icon fa-solid fa-heart"></i></div> */}
+            <div><i class="fa-solid fa-chevron-right" style={{color:'#d9d9d9'}}></i></div>
         </div>
     )
 }
