43f0f2a52e298e46c08c330cd53aae237f69bfed
kiimnj <1najin@naver.com>
2023-10-12 13:42:01 +0900

    remove: review/commentform.js

--- src/app/place/review/commentform.js
+++
@@ -1,8 +0,0 @@
-export default function CommentForm({contents}) {
-  return (
-    <form class='commentForm'>
-        <input id="commentInput" placeholder='댓글을 입력하세요.'></input>
-        <button id="commentButton">게시</button>
-    </form>
-  )
-}
