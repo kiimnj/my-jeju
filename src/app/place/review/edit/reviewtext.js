759a0d70dbdc8797747d88b0513dfe59a707cbb1
kiimnj <1najin@naver.com>
2023-10-12 09:54:00 +0900

    feat: review/edit/reviewtext.js

---
+++ src/app/place/review/edit/reviewtext.js
@@ -0,0 +1,8 @@
+export default function ReviewText() {
+    return (
+      <div id="reviewTextBox">
+        <textarea id="reviewText" name="reviewtext" placeholder="리뷰를 작성하세요."></textarea>
+        <span id="reviewTextCnts">25 / 500</span>
+      </div>
+    )
+  }
